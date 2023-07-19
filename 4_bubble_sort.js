39de98a6f8c2afa19a7be39425c277a751332374
Oleg <95434794+osemenets21@users.noreply.github.com>
2023-07-19 19:03:13 +0200

    Update 4_bubble_sort.js

--- 4_bubble_sort.js
+++ 4_bubble_sort.js
@@ -3,7 +3,7 @@ let count = 0
 
 function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
             if (array[j + 1] < array[j]) {
                 let tmp = array[j]
                 array[j] = array[j+1]
